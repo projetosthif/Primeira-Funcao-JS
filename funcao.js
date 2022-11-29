3b7304100b5cf036ca75e6832b57d0ba6fb328fc
Thifany Garcia <100360093+projetosthif@users.noreply.github.com>
2022-11-29 08:54:30 -0300

    Add files via upload

--- funcao.js
+++ funcao.js
@@ -1,3 +1,38 @@
 function soma(a,b){
     return a+b
 }
+
+function subtr(a,b){
+    return a-b
+}
+
+function div(a,b){
+    return a/b
+}
+
+function multi(a,b){
+    return a*b
+}
+
+function decisao(pri,seg,resp){
+    if(resp == 1){
+        resultado = soma(pri,seg)
+        document.write("<p>A soma é: " + resultado)
+    }
+
+    if(resp == 2){
+        resultado = subtr(pri,seg)
+        document.write("<p>A subtração é: " + resultado)
+    }
+
+    if(resp == 3){ 
+        resultado = div(pri,seg)
+        document.write("<p>A divisão é: " + resultado)
+    }
+
+    if(resp == 4){
+        resultado = multi(pri,seg)
+        document.write("<p>O produto é: " + resultado)
+    }
+
+}
